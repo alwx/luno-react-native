95686badca1b49c64e4ee7661fb03e8498f798c2
Artur Girenko <a.girenko@gmail.com>
2016-01-27 20:53:01 +0100

    delete files which should be ignored

--- target/android/luno/db.js
+++
@@ -1,8 +0,0 @@
-// Compiled by ClojureScript 1.7.170 {}
-goog.provide('luno.db');
-goog.require('cljs.core');
-goog.require('schema.core');
-luno.db.schema = new cljs.core.PersistentArrayMap(null, 5, [new cljs.core.Keyword(null,"android-drawer","android-drawer",-1076538023),schema.core.Any,new cljs.core.Keyword(null,"ios-tab","ios-tab",420742595),schema.core.Str,new cljs.core.Keyword(null,"data","data",-232669377),schema.core.Any,new cljs.core.Keyword(null,"error","error",-978969032),schema.core.Any,new cljs.core.Keyword(null,"loading?","loading?",1905707049),schema.core.Bool], null);
-luno.db.app_db = new cljs.core.PersistentArrayMap(null, 5, [new cljs.core.Keyword(null,"android-drawer","android-drawer",-1076538023),null,new cljs.core.Keyword(null,"ios-tab","ios-tab",420742595),"main",new cljs.core.Keyword(null,"data","data",-232669377),null,new cljs.core.Keyword(null,"error","error",-978969032),null,new cljs.core.Keyword(null,"loading?","loading?",1905707049),false], null);
-
-//# sourceMappingURL=db.js.map
--- target/ios/luno/android/routes.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.7.170 {}
-goog.provide('luno.android.routes');
-goog.require('cljs.core');
-luno.android.routes.routes = new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"main","main",-2117802661),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"name","name",1843675177),"main",new cljs.core.Keyword(null,"index","index",-1531685915),(1)], null),new cljs.core.Keyword(null,"about","about",1423892543),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"name","name",1843675177),"about",new cljs.core.Keyword(null,"index","index",-1531685915),(2)], null)], null);
-
-//# sourceMappingURL=routes.js.map
